a1b2b29a98fd6509e14c71408e795b8a7033e843
Thabata Pontes <tathabatapontes@hotmail.com>
2022-04-04 22:57:53 -0300

    Start library type class example

--- src/main/scala/TypeClasses/Library.scala
+++ src/main/scala/TypeClasses/Library.scala
@@ -1,7 +1,27 @@
 package TypeClasses
 
 object Library extends App {
+
+//  case class Genre(name: String)
+//  case class Location(name: String)
+//  case class Size(size: Int)
+  case class Public(name: String)
+  case class Private(name: String)
+
   trait Library[T]{
-    def genre(value: T): String
+    def getType(value: T): String
   }
+
+  def show[T](value: T)(implicit library: Library[T]): Unit = println(library.getType(value))
+
+  implicit val publicLibrary: Library[Public] = new Library[Public] {
+    def getType(value: Public): String = s"Type: ${value.name}"
+  }
+
+  implicit val privateLibrary: Library[Private] = new Library[Private] {
+    def getType(value: Private): String = s"Type: ${value.name}"
+  }
+
+  show(Public("public"))
+  show(Private("private"))
 }
