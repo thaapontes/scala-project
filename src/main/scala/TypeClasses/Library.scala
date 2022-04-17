220f93ddd0887f32270381eb9ce329a4cd59d579
Thabata Pontes <tathabatapontes@hotmail.com>
2022-04-17 15:35:58 -0300

    Create book case class

--- src/main/scala/TypeClasses/Library.scala
+++ src/main/scala/TypeClasses/Library.scala
@@ -4,26 +4,32 @@ package TypeClasses
 
 object Library extends App {
 
-//  case class Genre(name: String)
-//  case class Location(name: String)
-//  case class Size(size: Int)
-  case class Public(name: String)
-  case class Private(name: String)
+  case class Book(name: String) {
+    def authorName: String = name match {
+      case "Harry Potter" => "J.K Rowling"
+      case "Twilight" => "Stephenie Meyer"
+    }
+
+    def genreName: String = name match {
+      case "Harry Potter" => "adventure"
+      case "Twilight" => "fantasy"
+    }
+
+    def numberOfPages: Int = name match {
+      case "Harry Potter" => 300
+      case "Twilight" => 400
+    }
+  }
 
   trait Library[T]{
     def getType(value: T): String
   }
 
-  def show[T](value: T)(implicit library: Library[T]): Unit = println(library.getType(value))
+  def getBookInfo[T](value: T)(implicit library: Library[T]): Unit = println(library.getType(value))
 
-  implicit val publicLibrary: Library[Public] = new Library[Public] {
-    def getType(value: Public): String = s"Type: ${value.name}"
+  implicit val book: Library[Book] = new Library[Book] {
+    def getType(value: Book): String = s"Book: ${value.name}, Author: ${value.authorName}, Genre: ${value.genreName}, Number of Pages: ${value.numberOfPages}"
   }
 
-  implicit val privateLibrary: Library[Private] = new Library[Private] {
-    def getType(value: Private): String = s"Type: ${value.name}"
-  }
-
-  show(Public("public"))
-  show(Private("private"))
+  getBookInfo(Book("Harry Potter"))
 }
